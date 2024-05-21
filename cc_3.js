b8b2ac1db23f80c76addeced47904c72223cbaff
jessfraser8 <jfraser4@usf.edu>
2024-05-20 21:22:55 -0400

    Create Car class with constructor for make and model.

---
+++ cc_3.js
@@ -0,0 +1,10 @@
+//U09226075
+class Car {
+    constructor(make, speed){
+        this.make = make;
+        this.speed = speed;
+        console.log('The car is a ' + make + ' driving ' + speed + ' km/h.');
+    }
+
+
+}
