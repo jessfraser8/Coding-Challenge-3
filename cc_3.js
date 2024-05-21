f6fcb465e9aad888ee47cd2d264f0e01f2dd95f8
jessfraser8 <jfraser4@usf.edu>
2024-05-20 22:17:23 -0400

    Test acceleration and brake methods on Car instances.

--- cc_3.js
+++ cc_3.js
@@ -1,19 +1,39 @@
 //U09226075
+//Create Car class.
 class Car {
     constructor(make, speed){
         this.make = make;
         this.speed = speed;
-        console.log('The car is a ' + make + ' driving ' + speed + ' km/h.');
+        console.log(`The car is a ${make} driving ${speed} km/h.`);
     }
 
+//Add acceleration method.
     accelerate(){
-        return this.speed + 10
-        console.log('The new speed is ' + speed + ' km/h.')
+        this.speed = this.speed + 10;
+        console.log(`The new speed is ${this.speed} km/h.`);
+        return this.speed + 10;
     }
 
+//Add brake method.
     brake(){
-        return this.speed - 5
-        console.log('The new speed is ' + speed + ' km/h.')
+        this.speed = this.speed - 5;
+        console.log(`The new speed is ${this.speed} km/h.`);
+        return this.speed;
     }
-    
 }
+
+//Test Car objects.
+let car1 = new Car("BMW", 120);
+car1.brake();
+car1.brake();
+car1.accelerate();
+car1.brake();
+car1.accelerate();
+
+let car2 = new Car("Mercedes", 95);
+car2.accelerate();
+car2.accelerate();
+car2.brake();
+car2.accelerate();
+car2.brake();
+car2.brake();
